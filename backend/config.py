20cef3e3587a0e599461f3614690bdbfb3ad5751
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-05-07 10:39:18 +0300

    Delete backend/config.py

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = '7rW75KvUQrCudaU'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///pond_images.db'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://victoryf_vf_pond_color_trends:7rW75KvUQrCudaU@victoryfarmskenya.com:2083/victoryf_vf_pond_color_trends_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'static/uploads'
